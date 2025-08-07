61b4fcc4ed464a6a49f8ae909ac40036a8f29b0f
j2218s <zhaitengxiliu4@gmail.com>
2025-08-07 09:39:37 +0000

    chapter13

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,48 @@
 const express = require('express');
 const router = express.Router();
-let todos = [];
+const mysql = require('mysql');
 
-/* GET home page. */
+const connectionConfig = {
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'todo_app'
+};
+
 router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'ToDo App',
-    todos: todos,
-  });
+  const connection = mysql.createConnection(connectionConfig);
+  connection.query(
+    'select * from tasks;',
+    (error, results) => {
+      connection.end();
+      if (error) {
+        console.error('Error fetching tasks:', error);
+        return res.render('index', {
+          title: 'ToDo App (Error)',
+          todos: []
+        });
+      }
+      res.render('index', {
+        title: 'ToDo App',
+        todos: results,
+      });
+    }
+  );
 });
 
-/* POST home page. */
 router.post('/', function (req, res, next) {
+  const connection = mysql.createConnection(connectionConfig);
   const todo = req.body.add;
-  todos.push(todo);
-  res.redirect('/');
+  connection.query(
+    `insert into tasks (user_id, content) values (1, '${todo}');`,
+    (error, results) => {
+      connection.end();
+      if (error) {
+        console.error('Error inserting task:', error);
+      }
+      res.redirect('/');
+    }
+  );
 });
 
 module.exports = router;
